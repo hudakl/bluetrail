95363f67b73adcb66307782a7cb2488f0909241e
hudakl <hudak.laszlo.42@gmail.com>
2021-01-27 09:09:46 +0100

    BTP-0 Init project

---
+++ src/main/java/com/greyflame/bluetrail/BluetrailApplication.java
@@ -0,0 +1,13 @@
+package com.greyflame.bluetrail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluetrailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluetrailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greyflame/bluetrail/BluetrailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greyflame.bluetrail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluetrailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
