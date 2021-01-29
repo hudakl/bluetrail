a8124e44a13e364cae5146f9e7965de2a3ccb91a
hudakl <hudak.laszlo.42@gmail.com>
2021-01-29 15:23:58 +0100

    BTP-5 Basic language configuration

---
+++ src/main/java/com/greyflame/bluetrail/config/LanguageConfig.java
@@ -0,0 +1,37 @@
+package com.greyflame.bluetrail.config;
+
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+@ComponentScan(basePackages = "com.greyflame.bluetrail.config")
+public class LanguageConfig implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        Locale hunLocale = new Locale("hu", "HUNGARY");
+        slr.setDefaultLocale(hunLocale);
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}
