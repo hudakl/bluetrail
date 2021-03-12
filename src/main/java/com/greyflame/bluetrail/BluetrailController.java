afe03127f127d64a11d75e68955769716e19227d
hudakl <hudak.laszlo.42@gmail.com>
2021-03-12 12:14:06 +0100

    BTP-1.2 Fill stamp dropdowns

--- src/main/java/com/greyflame/bluetrail/BluetrailController.java
+++ src/main/java/com/greyflame/bluetrail/BluetrailController.java
@@ -1,7 +1,13 @@
 package com.greyflame.bluetrail;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.greyflame.bluetrail.persistence.LongDistanceTrail;
 import com.greyflame.bluetrail.persistence.LongDistanceTrailRepository;
+import com.greyflame.bluetrail.persistence.StampPoint;
+import com.greyflame.bluetrail.persistence.TrailSection;
+import com.greyflame.bluetrail.persistence.TrailSectionRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +15,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BluetrailController {
@@ -34,8 +43,26 @@ public class BluetrailController {
         return "result";
     }
 
+    @RequestMapping("/sectionchange")
+    @ResponseBody
+    public Map<Integer, String> sectionChange(
+        @RequestParam String section) {
+
+        Map<Integer, String> response = new HashMap<Integer, String>();
+        TrailSection trailSection = trailSectionRepository.findByOfficialId(section).get(0);
+
+        for (StampPoint stamp: trailSection.getStampPoints()) {
+            response.put(stamp.getNumber(), stamp.getName());
+        }
+
+        return response;
+    }
+
     @Autowired
     private LongDistanceTrailRepository ldtRepository;
+
+    @Autowired
+    private TrailSectionRepository trailSectionRepository;
 
     private LongDistanceTrail ldTrail;
 }
--- src/main/java/com/greyflame/bluetrail/persistence/TrailSectionRepository.java
+++ src/main/java/com/greyflame/bluetrail/persistence/TrailSectionRepository.java
@@ -1,7 +1,11 @@
 package com.greyflame.bluetrail.persistence;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface TrailSectionRepository extends CrudRepository<TrailSection, Integer> {
+
+    List<TrailSection> findByOfficialId(String officialId);
     
 }
